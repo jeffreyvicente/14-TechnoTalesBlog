c1dd33af42fcbc3356a90a8b43f1a46d90d39467
Jeffrey V <jeffreyvicente@hotmail.com>
2023-06-25 16:56:02 -0700

    Correct connection.js errors

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
-const Sequelize = require ('sequelize');
+// Loads environment variables from a .env file
 require('dotenv').config();
 
-let sequelize;
+// Importing the Sequelize library
+const Sequelize = require('sequelize');
 
-if(process.env.JAWSDB_URL){
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else { 
-    sequelize = new Sequelize(
-        process.env.DB_NMAE,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306
-        }
-    );
-}
+// Create a connection object
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  //Defines Database Name, Database User & Database Password
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      // Database location
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
+// Exports the Sequelize instance for use in other modules
 module.exports = sequelize;
