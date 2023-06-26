2f1be74094012807142ce6f795fe2195fb3e4526
Jeffrey V <jeffreyvicente@hotmail.com>
2023-06-25 21:27:50 -0700

    Added login and logout functions

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const express = require('express');
-const session = reqire('express-session');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-//const routes = require('./controllers');
-//const helpers = require('./utils/helpers');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection')
 const SequlizeStore = require('connect-session-sequelize')(session.Store);
@@ -27,3 +27,18 @@ const sess = {
         db: sequelize
     })
 };
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening on port 3001!!!'));
+});
